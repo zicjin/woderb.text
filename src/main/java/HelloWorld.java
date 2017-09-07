4a6fed5e9b08f10464aa1c29cf1b761aad2fa6fc
zicjin <zicjin@gmail.com>
2017-09-07 13:25:28 +0800

    .

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,6 +2,47 @@ import static spark.Spark.*;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        //  port(5678); <- listen to port 5678 instead of the default 4567
+
+        get("/hello", (request, response) -> "Hello World!");
+
+        // Will serve all static file are under "/public" in classpath if the route isn't consumed by others routes.
+        // When using Maven, the "/public" folder is assumed to be in "/main/resources"
+        staticFileLocation("/public");
+
+        //Running curl -i -H "Accept: application/json" http://localhost:4567/hello.json json message is read.
+        //Running curl -i -H "Accept: text/html" http://localhost:4567/hello.json HTTP 404 error is thrown.
+        get("/hello.json", "application/json", (request, response) -> "{\"message\": \"Hello World\"}");
+        get("/hello.json2", "application/json", (request, response) -> {
+            return new Books.Book("xxxx", "yyyy");
+        }, new JsonTransformer());
+
+        post("/hello", (request, response) ->
+                "Hello World: " + request.body()
+        );
+
+        get("/private", (request, response) -> {
+            response.status(401);
+            return "Go Away!!!";
+        });
+
+        get("/users/:name", (request, response) -> "Selected user: " + request.params(":name"));
+
+        get("/news/:section", (request, response) -> {
+            response.type("text/xml");
+            return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><news>" + request.params("section") + "</news>";
+        });
+
+        get("/protected", (request, response) -> {
+            halt(403, "I don't think so!!!");
+            return null;
+        });
+
+        get("/redirect", (request, response) -> {
+            response.redirect("/news/world");
+            return null;
+        });
+
+        get("/", (request, response) -> "root");
     }
 }
